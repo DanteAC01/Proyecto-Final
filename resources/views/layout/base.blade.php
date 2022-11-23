6e6468fe1bb4186eb7d450ccf605ddca0b510cb2
Dilberth12 <dilberthvalqui@gmail.com>
2022-11-23 18:41:45 -0500

    chevere

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -7,11 +7,6 @@
     <title>@yield('titulo')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<style>
-  img{
-    opacity: 0.7;
-  }
-</style>
 <body>
   <header class="mx-auto">
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
@@ -44,50 +39,95 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-
+    <div class="frases">
+      <p>
+        756E2070726F6772616D61646F7220616C206E61636572206E6F206C6C6F7261206469636520686F6C61206D756E646F
+      </p>
+    </div>
+    
   </header>
-    <nav class="navbar navbar-light navar-expand-* bg-light fixed-right">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </nav>
   @yield('cuerpo')
-
+<style>
+  .container{
+  width: 100%;
+  max-width: 100%;
+  justify-content: center;  
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;   
+  }
+  .container .card:hover{
+    transform: translateY(-15px);
+    margin: 15px;
+  }
+  .card{
+    margin: 15px;
+  }
+  .container .card img{
+    width: 330px;
+    height: 220px;
+    margin: auto;
+  }
+  .link{
+    text-decoration: none;
+    display: block;
+    color: black;
+    margin-left: 20px;
+  }
+  .uno{
+    width: 40%;
+    margin-right: 80px;
+  }
+  .dos{
+    width: 40%;
+  }
+  .h{
+    margin-top: 15px;
+  }
+  .hr1 {border: 3px solid #666; width: 95%; margin: auto; border-radius: 300px/10px; height: 0px; text-align: center;}
+  .hr {border: 0; height: 24px; box-shadow: inset 0 22px 12px -12px rgb(5, 134, 117); color: rgb(22, 15, 129); text-align: center;}
+  .hr:after {content:"\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605\2600\2605"; font-size: 14px;color: rgba(226, 171, 171, 0.123);}
+</style>
+<hr class="hr1">
+  <div class="container">
+    <div class="card">
+      <img src="https://fondosmil.com/fondo/26250.jpg" class="card-img" alt="...">
+    </div>
+    <div class="card">
+      <img src="https://p4.wallpaperbetter.com/wallpaper/782/64/1007/epica-the-holograpic-principle-wallpaper-preview.jpg" class="card-img" alt="...">
+    </div>
+    <div class="card">
+      <img src="https://dam.smashmexico.com.mx/wp-content/uploads/2018/08/29203205/marvel-deadpool-metidas-de-pata-top5-cover.jpg" class="card-img" alt="...">
+    </div>
+    <div class="card">
+      <img src="https://freakuotes.com/users/stacks/594-epicas-d55d68f783529ac3f7b2833caa28ba70e085f251.jpg" class="card-img" alt="...">
+    </div>
+    <div class="card">
+      <img src="https://www.todogadget.net/files/12-2021/ad62476/fondos-de-pantalla-super-epicos-usd-1884998394_large.jpg" class="card-img" alt="...">
+    </div>
+    <div class="card">
+      <img src="https://w0.peakpx.com/wallpaper/735/899/HD-wallpaper-fenix-epico-fantasia-leyenda.jpg" class="card-img" alt="...">
+    </div>
+  </div>
+  <hr class="hr1">
+  <div class="container">
+    <div class="uno">
+      <h4 class="h">Linkcografía</h4>
+      <hr class="hr">
+        <a class="link" href="https://www.youtube.com/watch?v=JDs45GmZWUo"><i class="fas fa-link"></i>Boostrap</a>
+        <a class="link" href="https://lenguajehtml.com/html/"><i class="fas fa-link"></i>Html</a>
+        <a class="link" href="https://www.google.com/search?q=imagenes+epicas&rlz=1C1CHBF_esPE1032PE1032&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjuyIHzoMX7AhWuGbkGHVu9DZ0Q_AUoAXoECAEQAw&biw=1304&bih=665&dpr=1"><i class="fas fa-link"></i>Imagenes</a>
+        <a class="link" href="https://rockcontent.com/es/blog/bootstrap/"><i class="fas fa-link"></i>Informacion</a>
+    </div>
+    <div class="dos">
+      <h4 class="h">Redes Sociales</h4>
+      <hr class="hr">
+      <a class="link" href="https://www.youtube.com/">YouTube</a>
+      <a class="link" href="https://www.facebook.com/">Facebook</a>
+      <a class="link" href="hhttps://twitter.com/">Twitter</a>
+      <a class="link" href="https://www.instagram.com/">Instagram</a>
+    </div>
+  </div>
   <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
