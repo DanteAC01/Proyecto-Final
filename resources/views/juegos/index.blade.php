6313efbf271a2999b46eedb570d2c29009c7f9b7
unknown <carlos.oxidentexd@gmail.com>
2022-11-24 01:43:43 -0500

    araara

--- resources/views/juegos/index.blade.php
+++ resources/views/juegos/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.base')
 @section('titulo', 'Inicio')
 @section('cuerpo')
-hola
+<p>AQUI IRA ESA VAINA DE LA ROCHIS</p>
+<p>DSABFJKASD</p>
+<p>AEAEAE</p>
+<p>GAAAAAAAAAAAAAAA</p>
+<p>XD</p>
 @endsection
